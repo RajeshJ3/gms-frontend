510ec7ed6e964dcc0a50b5f8509e7a7b469e58a6
Rajesh Joshi <joshirajesh448@gmail.com>
2021-02-11 23:27:27 +0530

    added Progress

--- src/pages/Memberships.js
+++ src/pages/Memberships.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import CardList from '../components/Memberships/CardList'
+import React from "react";
+import CardList from "../components/Memberships/CardList";
 import Title from "../components/Typographies/Title";
+import Progress from "../components/errors/Progress";
 
-export default function Index() {
-  return (
-    <React.Fragment>
+export default function Index(props) {
+  return props.isAuthenticated ? (
+    <>
       <Title title="Memberships" />
       <CardList />
-    </React.Fragment>
+    </>
+  ) : (
+    <Progress />
   );
 }
-

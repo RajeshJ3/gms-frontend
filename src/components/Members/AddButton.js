2827d3acb4ac2703097b87a37dff555bc9689fb0
Rajesh Joshi <joshirajesh448@gmail.com>
2021-02-13 15:55:36 +0530

    changes

--- src/components/Members/AddButton.js
+++ src/components/Members/AddButton.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import { Add } from "@material-ui/icons";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import Avatar from "@material-ui/core/Avatar";
+import { IconButton } from "@material-ui/core";
+import { AddCircle } from "@material-ui/icons";
+import { DOMAIN, getToken, getGymId } from "../../store/utility";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    marginBottom: "30px",
     display: "flex",
     justifyContent: "center",
     [theme.breakpoints.down("sm")]: {
       marginTop: "22px",
-      marginBottom: "25px",
     },
   },
   dialog: {
@@ -34,14 +34,55 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 export default function AddButton(props) {
   const classes = useStyles();
 
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState({});
+  const [success, setSuccess] = React.useState(true);
+  const [created, setCreated] = React.useState(false);
+  const [batch, setBatch] = React.useState("");
+  const [batches, setBatches] = React.useState([]);
+
+
   const [open, setOpen] = React.useState(false);
-  const [name, setName] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
   const [email, setEmail] = React.useState("");
-  const [mobile, setMobile] = React.useState("");
-  const [batch, setBatch] = React.useState("morning");
+
+
+  // Batch
+  React.useEffect(() => {
+    axios({
+      method: "GET",
+      url: `${DOMAIN}/gyms/batches/`,
+      headers: { Authorization: `Token ${getToken}` },
+      params: { gym: getGymId },
+    })
+      .then((res) => {
+        let temp = res.data.output.map((i) => ({
+          value: i.id,
+          label: i.title,
+        }));
+        setBatches([{ value: -1, label: "Select.." }, ...temp]);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err.request);
+        setLoading(false);
+      });
+  }, []);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -51,34 +92,39 @@ export default function AddButton(props) {
     setOpen(false);
   };
 
-  const batches = [
-    {
-      value: "morning",
-      label: "Morning",
-    },
-    {
-      value: "noon",
-      label: "Noon",
-    },
-    {
-      value: "evening",
-      label: "Evening",
-    },
-  ];
+  const getOrCreateUser = (e) => {
+    e.preventDefault();
+    let pswd = makeid(10) + "@" + makeid(10);
+    axios
+      .post(`${DOMAIN}/auth/registration/`, {
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        password1: pswd,
+        password2: pswd,
+        user_type: "member",
+      })
+      .then((res) => {
+        setCreated(true);
+        setSuccess(true);
+        setLoading(false);
+      })
+      .catch((err) => {
+        let errorExist = JSON.parse(err.request.response);
+        setError(errorExist);
+        console.log(errorExist);
+        if (errorExist.email) {
+          setSuccess(true);
+        }
+        setLoading(false);
+      });
+  };
 
   return (
     <div className={classes.root}>
-      <Button
-        onClick={handleClickOpen}
-        variant="contained"
-        className={classes.button}
-        color="primary"
-        size="small"
-        disableElevation
-        startIcon={<Add />}
-      >
-        {props.title}
-      </Button>
+      <IconButton aria-label="add" onClick={handleClickOpen} size="small">
+        <AddCircle color="primary" />
+      </IconButton>
       <Dialog
         open={open}
         onClose={handleClose}
@@ -86,19 +132,23 @@ export default function AddButton(props) {
       >
         <DialogTitle id="form-dialog-title">Add member</DialogTitle>
         <DialogContent className={classes.dialog}>
-          <div className={classes.imageWrapper}>
-            <Avatar alt={name} src="https://material-ui.com/static/images/avatar/2.jpg" />
-            <Button variant="outlined" size="small" color="primary">Upload</Button>
-          </div>
           <TextField
             margin="dense"
-            id="name"
-            label="Name"
+            id="firstName"
+            label="First Name"
             type="text"
             fullWidth
-            autoComplete={false}
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            id="lastName"
+            label="Last Name"
+            type="text"
+            fullWidth
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
           />
           <TextField
             margin="dense"
@@ -106,19 +156,8 @@ export default function AddButton(props) {
             label="Email Address"
             type="email"
             fullWidth
-            autoComplete={false}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            margin="dense"
-            id="mobile"
-            label="Mobile"
-            type="number"
-            fullWidth
-            autoComplete={false}
-            value={mobile}
-            onChange={(e) => setMobile(e.target.value)}
           />
           <TextField
             id="batch"
@@ -143,8 +182,8 @@ export default function AddButton(props) {
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
-            Save
+          <Button color="primary" onClick={getOrCreateUser} disabled={loading}>
+            {loading ? "Creating" : "Next"}
           </Button>
         </DialogActions>
       </Dialog>
